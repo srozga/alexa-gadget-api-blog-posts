dd908e99d27f913c277fadde180939459b50fc34
Szymon Rozga <szymon.rozga@gmail.com>
2018-10-20 13:39:41 -0400

    removing unused file

--- lambda/custom/src/helpers/SetLightBuilder.ts
+++
@@ -1,7 +0,0 @@
-import { services, interfaces } from "ask-sdk-model";
-
-export class SetLightBuilder {
-    export function buildSetLightDirection(): interfaces.gadgetController.SetLightDirective {
-
-    }
-}
